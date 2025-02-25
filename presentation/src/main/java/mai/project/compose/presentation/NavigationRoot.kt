1d6a2f93b26dc468000cd5e8c65374584b9787b9
DDPlay123 <a70325000@gmail.com>
2025-02-25 14:54:44 +0800

    add 2-5-8

--- presentation/src/main/java/mai/project/compose/presentation/NavigationRoot.kt
+++ presentation/src/main/java/mai/project/compose/presentation/NavigationRoot.kt
@@ -28,6 +28,7 @@ import mai.project.compose.presentation.ui.course_2.Course_2_5_4_ScreenRoot
 import mai.project.compose.presentation.ui.course_2.Course_2_5_5_ScreenRoot
 import mai.project.compose.presentation.ui.course_2.Course_2_5_6_ScreenRoot
 import mai.project.compose.presentation.ui.course_2.Course_2_5_7_ScreenRoot
+import mai.project.compose.presentation.ui.course_2.Course_2_5_8_ScreenRoot
 
 private const val ROUTE_HOME = "ROUTE_HOME"
 
@@ -83,6 +84,7 @@ private fun NavGraphBuilder.homeGraph(
                     "2-5-5" -> Course_2_5_5_ScreenRoot()
                     "2-5-6" -> Course_2_5_6_ScreenRoot()
                     "2-5-7" -> Course_2_5_7_ScreenRoot()
+                    "2-5-8" -> Course_2_5_8_ScreenRoot()
                     // TODO Course 3
                     // TODO Course 4
                     // TODO Course 5
